6d4c8f728210849fa7cd54df0b54e23ae82ab9d3
Anjana-Heshan <anjana.h@boswingroup.com>
2026-08-15 21:20:04 +0530

    Add production PM2 services configuration

---
+++ ecosystem-services.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  apps: [
+    {
+      name: "menu-service",
+      script: "java",
+      args: "-jar -Dspring.profiles.active=gcp -Dspring.config.import=optional:configserver:http://10.0.1.10:8888 menu-service-0.0.1-SNAPSHOT.jar",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M"
+    },
+    {
+      name: "order-service",
+      script: "java",
+      args: "-jar -Dspring.profiles.active=gcp -Dspring.config.import=optional:configserver:http://10.0.1.10:8888 order-service-0.0.1-SNAPSHOT.jar",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M"
+    },
+    {
+      name: "delivery-service",
+      script: "java",
+      args: "-jar -Dspring.profiles.active=gcp -Dspring.config.import=optional:configserver:http://10.0.1.10:8888 delivery-service-0.0.1-SNAPSHOT.jar",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M"
+    },
+    {
+      name: "payment-service",
+      script: "java",
+      args: "-jar -Dspring.profiles.active=gcp -Dspring.config.import=optional:configserver:http://10.0.1.10:8888 payment-service-0.0.1-SNAPSHOT.jar",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M"
+    }
+  ]
+};
